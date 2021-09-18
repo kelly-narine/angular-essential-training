8d4654f968d0077e367ab58c4dc24df688e1fe79
Kelly Narine <knarine12@hotmail.ca>
2021-09-18 18:04:23 -0400

    main.ts added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"; //function import
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"; //function import.
 import { AppModule } from "./app/app.module";
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule); // allows the app to run in the browser
